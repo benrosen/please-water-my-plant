e8c6bb986949bca12675ec403faf26b03dd0add3
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-05 14:18:50 -0400

    create gravity module

---
+++ src/gravity/gravity.ts
@@ -0,0 +1,9 @@
+import {Acceleration} from "../acceleration";
+
+/**
+ * A constant value that represents the force of gravity.
+ */
+export const Gravity: Acceleration = {
+  x: 0,
+  y: -9.8,
+};
---
+++ src/gravity/index.ts
@@ -0,0 +1,1 @@
+export { Gravity } from "./gravity";
