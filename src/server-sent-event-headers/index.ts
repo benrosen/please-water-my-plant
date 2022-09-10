96cff1fa80d850d24e684a9ddc7e4d0105b5b0d8
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-10 10:32:08 -0400

    create server-sent-event-headers module

---
+++ src/server-sent-event-headers/index.ts
@@ -0,0 +1,1 @@
+export { ServerSentEventHeaders } from "./server-sent-event-headers";
---
+++ src/server-sent-event-headers/server-sent-event-headers.ts
@@ -0,0 +1,8 @@
+/**
+ * HTTP headers for server-sent events.
+ */
+export const ServerSentEventHeaders = {
+  "Cache-Control": "no-cache",
+  "Content-Type": "text/event-stream",
+  Connection: "keep-alive",
+};
