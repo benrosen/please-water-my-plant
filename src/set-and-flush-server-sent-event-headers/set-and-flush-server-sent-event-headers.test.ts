bcbe567589b50003539f76cc489d76aefe4038e0
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-11 11:38:18 -0400

    test set-and-flush-server-sent-event-headers module

--- src/set-and-flush-server-sent-event-headers/set-and-flush-server-sent-event-headers.test.ts
+++ src/set-and-flush-server-sent-event-headers/set-and-flush-server-sent-event-headers.test.ts
@@ -1,24 +1,58 @@
-// mock response.flushHeaders
-// mock response.set
+import { Response } from "express";
+import { ServerSentEventHeaders } from "../server-sent-event-headers";
+import { setAndFlushServerSentEventHeaders } from "./set-and-flush-server-sent-event-headers";
+
+const mockFlushHeaders = jest.fn();
+
+const mockSet = jest.fn();
+
+const mockResponse = {
+  flushHeaders: mockFlushHeaders,
+  set: mockSet,
+} as unknown as Response;
 
 describe("The setAndFlushServerSentEventHeaders function", () => {
   describe("should call", () => {
     test("response.set with ServerSentEventHeaders", () => {
-      expect(true).toStrictEqual(false);
+      setAndFlushServerSentEventHeaders({
+        response: mockResponse,
+      });
+
+      expect(mockSet).toHaveBeenCalledWith(ServerSentEventHeaders);
     });
 
     test("response.flushHeaders", () => {
-      expect(true).toStrictEqual(false);
+      setAndFlushServerSentEventHeaders({
+        response: mockResponse,
+      });
+
+      expect(mockFlushHeaders).toHaveBeenCalled();
     });
   });
 
   describe("should throw", () => {
     test("when response.set throws", () => {
-      expect(true).toStrictEqual(false);
+      mockSet.mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+      expect(() => {
+        setAndFlushServerSentEventHeaders({
+          response: mockResponse,
+        });
+      }).toThrow();
     });
 
     test("when response.flushHeaders throws", () => {
-      expect(true).toStrictEqual(false);
+      mockFlushHeaders.mockImplementationOnce(() => {
+        throw new Error()
+      })
+      
+      expect(() => {
+        setAndFlushServerSentEventHeaders({
+          response: mockResponse,
+        });
+      }).toThrow();
     });
   });
 });
