02d22c1b941aa45f79cae3748e2f23215535d7bf
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-12 09:56:16 -0400

    use respondWithStatusCode module

--- src/respond-with-internal-server-error/respond-with-internal-server-error.ts
+++ src/respond-with-internal-server-error/respond-with-internal-server-error.ts
@@ -1,17 +1,19 @@
 import {Response} from "express";
 import {InternalServerErrorStatusCode} from "../internal-server-error-status-code";
+import {respondWithStatusCode} from "../respond-with-status-code";
 
 /**
  * Send an {@link InternalServerErrorStatusCode} to the client via the given {@link Response}.
  * @param params The config options
- * @param params.response The {@link Response} with which to send a 500 status code to the client.
+ * @param params.response The {@link Response} with which to send an {@link InternalServerErrorStatusCode} to the client.
  */
 export const respondWithInternalServerError = ({
   response,
 }: {
   response: Response;
 }) => {
-  response.status(InternalServerErrorStatusCode);
-
-  response.send();
+  respondWithStatusCode({
+    response,
+    statusCode: InternalServerErrorStatusCode,
+  });
 };
