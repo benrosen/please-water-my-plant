2445e788af4c66c8ccda47d828ba51951c17720c
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-11 20:45:00 -0400

    create accepted-status-code module

---
+++ src/accepted-status-code/accepted-status-code.ts
@@ -0,0 +1,4 @@
+/**
+ * Indicates that the request has been accepted for processing, but the processing has not been completed; in fact, processing may not have started yet. The request might or might not eventually be acted upon, as it might be disallowed when processing actually takes place.
+ */
+export const AcceptedStatusCode = 202;
---
+++ src/accepted-status-code/index.ts
@@ -0,0 +1,1 @@
+export { AcceptedStatusCode } from "./accepted-status-code";
