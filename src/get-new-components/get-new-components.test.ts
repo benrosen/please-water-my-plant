61080cab6e2d717336b02e445e7028a9fe3b5904
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-24 16:31:18 -0400

    create get-new-components module

---
+++ src/get-new-components/get-new-components.test.ts
@@ -0,0 +1,15 @@
+import { ComponentIndex } from "../component-index";
+import { getNewComponents } from "./get-new-components";
+
+describe("The getNewComponents function", () => {
+  it("should return an array", () => {
+    const componentIndex: ComponentIndex = {
+      byComponentId: {},
+      byEntityId: {},
+    };
+
+    const result = getNewComponents({ componentIndex });
+
+    expect(Array.isArray(result)).toStrictEqual(true);
+  });
+});
---
+++ src/get-new-components/index.ts
@@ -0,0 +1,1 @@
+export { getNewComponents } from "./get-new-components";
