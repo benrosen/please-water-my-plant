2243e2e62dc5187f9f97e31cf2228da763960a3a
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-24 19:45:09 -0400

    create home-page module

---
+++ src/home-page/home-page.ts
@@ -0,0 +1,11 @@
+export const HomePage = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Monster Mashup</title>
+</head>
+<body>
+<h1>Monster Mashup</h1>
+<p>Hello world!</p>
+</body>
+</html>`;
