f40c677e17687140045b28dd9d8c997b84d283d1
Ben Rosen <benrosen@users.noreply.github.com>
2022-09-29 17:24:36 -0400

    add link to accepted status code http reference

--- src/accepted-status-code/accepted-status-code.ts
+++ src/accepted-status-code/accepted-status-code.ts
@@ -1,4 +1,5 @@
 /**
  * Indicates that the request has been accepted for processing, but the processing has not been completed; in fact, processing may not have started yet. The request might or might not eventually be acted upon, as it might be disallowed when processing actually takes place.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/202
  */
 export const AcceptedStatusCode = 202;
