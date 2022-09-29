a57ecdaac647027a37ae7ea0beb59df5fd9201f5
Ben Rosen <benrosen@users.noreply.github.com>
2022-09-29 17:38:27 -0400

    add link to internal server error status code http reference

--- src/internal-server-error-status-code/internal-server-error-status-code.ts
+++ src/internal-server-error-status-code/internal-server-error-status-code.ts
@@ -1,4 +1,5 @@
 /**
  * Indicates that the server encountered an unexpected condition that prevented it from fulfilling the request.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500
  */
 export const InternalServerErrorStatusCode = 500;
