b977b28ad72c8fc71a2994a3ad34f26e0bebf81c
Ben Rosen <benrosen@users.noreply.github.com>
2022-09-29 17:27:11 -0400

    add link to bad request error status code http reference

--- src/bad-request-error-status-code/bad-request-error-status-code.ts
+++ src/bad-request-error-status-code/bad-request-error-status-code.ts
@@ -1,4 +1,5 @@
 /**
  * Indicates that the server cannot or will not process the request due to something that is perceived to be a client error (for example, malformed request syntax, invalid request message framing, or deceptive request routing).
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400
  */
 export const BadRequestErrorStatusCode = 400;
