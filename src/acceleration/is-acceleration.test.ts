8748aebad120a0fc44d9e4443d5a89ea8744d925
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-29 17:17:29 -0400

    add more valid examples of objects that implement the acceleration interface

--- src/acceleration/is-acceleration.test.ts
+++ src/acceleration/is-acceleration.test.ts
@@ -1,6 +1,16 @@
 import { isAcceleration } from "./is-acceleration";
 
-const validExamples = [{ x: 0, y: 0 }];
+const validExamples = [
+  { x: 0, y: 0 },
+  { x: 1, y: 0 },
+  { x: 0, y: 1 },
+  { x: 1, y: 1 },
+  { x: -1, y: 0 },
+  { x: 0, y: -1 },
+  { x: -1, y: -1 },
+  { x: 98765.4, y: 12.3456 },
+  { x: -567.89, y: 8 },
+];
 
 const invalidExamples = [{ entity: "foo" }, { id: "bar" }];
 
