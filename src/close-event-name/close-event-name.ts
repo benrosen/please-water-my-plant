9e74e4ea66e04386bd19ed3d884eb620c756b219
Ben Rosen <benrosen@users.noreply.github.com>
2022-09-29 17:33:38 -0400

    document close event name constant

--- src/close-event-name/close-event-name.ts
+++ src/close-event-name/close-event-name.ts
@@ -1,1 +1,4 @@
+/**
+ * Indicates that the request is completed, or its underlying connection was terminated prematurely (before the response completion).
+ */
 export const CloseEventName = "close";
