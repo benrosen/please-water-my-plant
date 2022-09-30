d6857f4daaa2419785793e57277b0147aeee0895
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-29 20:28:53 -0400

    reorganize isTimestamped test to use standardized structure

--- src/timestamped/is-timestamped.test.ts
+++ src/timestamped/is-timestamped.test.ts
@@ -1,21 +1,25 @@
 import { isTimestamped } from "./is-timestamped";
 
+const validExamples = [{ timestamp: 0 }];
+
+const invalidExamples = [{id: 0}]
+
 describe("The isTimestamped type-guard function", () => {
   describe("should return true", () => {
-    test.each([{ timestamp: 0 }])(
-      "when the given value, %j, implements the isTimestamped interface",
-      (validValue) => {
-        expect(isTimestamped(validValue)).toBeTruthy();
-      }
+    test.each(validExamples)(
+        "when the given value, %j, implements the isTimestamped interface",
+        (validValue) => {
+          expect(isTimestamped(validValue)).toBeTruthy();
+        }
     );
   });
 
   describe("should return false", () => {
-    test.each([{ id: 0 }])(
-      "when the given value, %j, does NOT implement the isTimestamped interface",
-      (invalidValue) => {
-        expect(isTimestamped(invalidValue)).toBeFalsy();
-      }
+    test.each(invalidExamples)(
+        "when the given value, %j, does NOT implement the isTimestamped interface",
+        (invalidValue) => {
+          expect(isTimestamped(invalidValue)).toBeFalsy();
+        }
     );
   });
 });
