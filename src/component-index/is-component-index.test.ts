17b8601a19f065bcbfc844d80be3080e7985c143
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-29 20:13:41 -0400

    reorganize isComponentIndex test to use standardized structure

--- src/component-index/is-component-index.test.ts
+++ src/component-index/is-component-index.test.ts
@@ -1,32 +1,41 @@
 import { isComponentIndex } from "./is-component-index";
 
+const validExamples = [
+  {
+    byComponentId: {
+      ["7d1e8ee0-3310-44db-9c8c-9a9556a30bca"]: {
+        id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+        entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
+      },
+    },
+    byEntityId: {
+      ["2c2a0e29-98c5-43b1-a452-91eca64a8ba8"]: {
+        id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+        entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
+      },
+    },
+  },
+];
+
+const invalidExamples = [false];
+
 describe("The isComponentIndex function", () => {
   describe("should return true", () => {
-    test.each([
-      {
-        byComponentId: {
-          ["7d1e8ee0-3310-44db-9c8c-9a9556a30bca"]: {
-            id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
-            entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
-          },
-        },
-        byEntityId: {
-          ["2c2a0e29-98c5-43b1-a452-91eca64a8ba8"]: {
-            id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
-            entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
-          },
-        },
-      },
-    ])("when the given value, %j, is a valid component index", (validValue) => {
-      expect(isComponentIndex(validValue)).toBeTruthy();
-    });
+    test.each(validExamples)(
+      "when the given value, %j, implements the ComponentIndex interface",
+      (validValue) => {
+        const result = isComponentIndex(validValue);
+        expect(result).toBeTruthy();
+      }
+    );
   });
 
   describe("should return false", () => {
-    test.each([false])(
-      "when the given value, %j, is not a valid component index",
+    test.each(invalidExamples)(
+      "when the given value, %j, does not implement the ComponentIndex interface",
       (invalidValue) => {
-        expect(isComponentIndex(invalidValue)).toBeFalsy();
+        const result = isComponentIndex(invalidValue);
+        expect(result).toBeFalsy();
       }
     );
   });
