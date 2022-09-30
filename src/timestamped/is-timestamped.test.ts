b60976a663e426bc6991dc5169e1b8a26e643a51
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-09-29 21:11:26 -0400

    use toStrictEqual instead of toBeTruthy and toBeFalsy

--- src/timestamped/is-timestamped.test.ts
+++ src/timestamped/is-timestamped.test.ts
@@ -2,24 +2,24 @@ import { isTimestamped } from "./is-timestamped";
 
 const validExamples = [{ timestamp: 0 }];
 
-const invalidExamples = [{id: 0}]
+const invalidExamples = [{ id: 0 }];
 
 describe("The isTimestamped type-guard function", () => {
   describe("should return true", () => {
     test.each(validExamples)(
-        "when the given value, %j, implements the isTimestamped interface",
-        (validValue) => {
-          expect(isTimestamped(validValue)).toBeTruthy();
-        }
+      "when the given value, %j, implements the isTimestamped interface",
+      (validValue) => {
+        expect(isTimestamped(validValue)).toStrictEqual(true);
+      }
     );
   });
 
   describe("should return false", () => {
     test.each(invalidExamples)(
-        "when the given value, %j, does NOT implement the isTimestamped interface",
-        (invalidValue) => {
-          expect(isTimestamped(invalidValue)).toBeFalsy();
-        }
+      "when the given value, %j, does NOT implement the isTimestamped interface",
+      (invalidValue) => {
+        expect(isTimestamped(invalidValue)).toStrictEqual(false);
+      }
     );
   });
 });
