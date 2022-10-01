ef8f8150c187d1bfe8b21b11f81312ff222a9fde
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-01 17:48:48 -0400

    update documentation to match description of component from wikipedia

--- src/component/component.ts
+++ src/component/component.ts
@@ -2,6 +2,7 @@ import {EntityRelated} from "../entity-related";
 import {IdentifiedByGuid} from "../identified-by-guid";
 
 /**
- * A uniquely-identified object that confers behavior to a related entity.
+ * A component labels an entity as possessing a particular aspect, and holds the data needed to model that aspect.
+ * @see https://en.wikipedia.org/wiki/Entity_component_system#Characteristics
  */
 export interface Component extends IdentifiedByGuid, EntityRelated {}
