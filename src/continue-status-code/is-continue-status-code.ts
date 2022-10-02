1307062a4c1644615b2f9682e0a34b56281fa146
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 18:00:24 -0400

    update type guard to check against constant

--- src/continue-status-code/is-continue-status-code.ts
+++ src/continue-status-code/is-continue-status-code.ts
@@ -1,4 +1,4 @@
-import {ContinueStatusCode} from "./continue-status-code";
+import {CONTINUE_STATUS_CODE, ContinueStatusCode} from "./continue-status-code";
 
 /**
  * Return `true` if the given value (of `unknown` type) is of type {@link ContinueStatusCode}. Otherwise, return `false`.
@@ -7,5 +7,5 @@ import {ContinueStatusCode} from "./continue-status-code";
 export const isContinueStatusCode = (
   value: unknown
 ): value is ContinueStatusCode => {
-  return typeof value === "number" && value === 100;
+  return typeof value === "number" && value === CONTINUE_STATUS_CODE;
 };
