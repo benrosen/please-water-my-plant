1f064cb5114cea092dd5397c0392b32085009578
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 18:49:09 -0400

    create partial-content-status-code module

---
+++ src/partial-content-status-code/partial-content-status-code.ts
@@ -0,0 +1,11 @@
+/**
+ * This response code is used when the Range header is sent from the client to request only part of a resource.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/206
+ */
+export type PartialContentStatusCode = 206;
+
+/**
+ * This response code is used when the Range header is sent from the client to request only part of a resource.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/206
+ */
+export const PARTIAL_CONTENT_STATUS_CODE: PartialContentStatusCode = 206;
