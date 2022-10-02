1175a7b2602c545281158f6db7f9e22c5311b361
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-01 22:01:13 -0400

    add more valid and invalid examples for is identifier tests

--- src/identifier/is-identifier.test.ts
+++ src/identifier/is-identifier.test.ts
@@ -1,8 +1,14 @@
 import { isIdentifier } from "./is-identifier";
 
-const validExamples = ["C63B397E-AF40-43C3-81C8-3F14A86EB7EE"];
+const validExamples = [
+  "C63B397E-AF40-43C3-81C8-3F14A86EB7EE",
+  "7547F144-6E23-482D-B83F-477A32A49D3C",
+  "7F4DD28D-23E8-46FD-AEDB-FBB9923DE078",
+  "6A114A5C-87C1-40BF-A408-DAD2382C5DF9",
+  "17887E2B-F2BA-481E-BDDA-70566B368AA7",
+];
 
-const invalidExamples = [""];
+const invalidExamples = ["", false, { foo: "bar" }, 123, ["foo", "bar"]];
 
 describe("The isIdentifier type-guard function", () => {
   describe("should return true", () => {
