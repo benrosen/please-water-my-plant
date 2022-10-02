5681e54affe993a85e72c1537699016859a839c3
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 08:38:57 -0400

    add more valid and invalid examples for is entity related tests

--- src/entity-related/is-entity-related.test.ts
+++ src/entity-related/is-entity-related.test.ts
@@ -1,8 +1,20 @@
 import { isEntityRelated } from "./is-entity-related";
 
-const validExamples = [{ entityId: "E99B677C-2C29-46B6-91D1-A57296699EC8" }];
+const validExamples = [
+  { entityId: "D65EE2AF-5AF8-47E7-AA40-90365E6DC9FA" },
+  { entityId: "94FED290-67F7-4D10-A1B8-A701FEFB0F06" },
+  { entityId: "B71F0A75-879E-4393-93D2-A22570DB1958" },
+  { entityId: "925A38BE-379F-4AC1-A6A4-A1F01FBF2528" },
+  { entityId: "4DE26955-5C17-4DBE-8C24-B5611D70AD36" },
+];
 
-const invalidExamples = [{ entity: "foo" }, { id: "bar" }];
+const invalidExamples = [
+  { entity: "foo" },
+  { id: "bar" },
+  { entityId: "" },
+  { entityId: false },
+  { entityId: "4DE26955-5C17-4DBE" },
+];
 
 describe("The isEntityRelated type-guard function", () => {
   describe("should return true", () => {
