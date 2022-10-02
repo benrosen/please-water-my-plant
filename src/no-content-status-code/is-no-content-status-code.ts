2416bdc456083a8bdbcef8c5178994fa4cc9b7a1
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 18:44:34 -0400

    create non-authoritative-information-status-code module

---
+++ src/no-content-status-code/is-no-content-status-code.ts
@@ -0,0 +1,11 @@
+import {NO_CONTENT_STATUS_CODE, NoContentStatusCode,} from "./no-content-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link NoContentStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isNoContentStatusCode = (
+  value: unknown
+): value is NoContentStatusCode => {
+  return typeof value === "number" && value === NO_CONTENT_STATUS_CODE;
+};
---
+++ src/no-content-status-code/no-content-status-code.ts
@@ -0,0 +1,13 @@
+/**
+ * There is no content to send for this request, but the headers may be useful.
+ * The user agent may update its cached headers for this resource with the new ones.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
+ */
+export type NoContentStatusCode = 204;
+
+/**
+ * There is no content to send for this request, but the headers may be useful.
+ * The user agent may update its cached headers for this resource with the new ones.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
+ */
+export const NO_CONTENT_STATUS_CODE: NoContentStatusCode = 204;
