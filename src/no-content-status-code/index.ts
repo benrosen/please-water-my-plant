fdd0aeda3a7e5caf9d47250290a09ee682aa8cd9
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 18:44:34 -0400

    create non-authoritative-information-status-code module

---
+++ src/no-content-status-code/index.ts
@@ -0,0 +1,2 @@
+export { NoContentStatusCode } from "./no-content-status-code";
+export { isNoContentStatusCode } from "./is-no-content-status-code";
---
+++ src/no-content-status-code/no-content-status-code.test.ts
@@ -0,0 +1,28 @@
+import { isNoContentStatusCode } from "./is-no-content-status-code";
+import { NO_CONTENT_STATUS_CODE } from "./no-content-status-code";
+
+const validExamples = [NO_CONTENT_STATUS_CODE];
+
+const invalidExamples = [0, 1, 101, 1001, 200, 404];
+
+describe("The isNoContentStatusCode type-guard function", () => {
+  describe("should return true", () => {
+    test.each(validExamples)(
+      "when the given value, %j, implements the NoContentStatusCode interface",
+      (validExample) => {
+        const result = isNoContentStatusCode(validExample);
+        expect(result).toBeTruthy();
+      }
+    );
+  });
+
+  describe("should return false", () => {
+    test.each(invalidExamples)(
+      "when the given value, %j, does not implement the NoContentStatusCode interface",
+      (invalidExample) => {
+        const result = isNoContentStatusCode(invalidExample);
+        expect(result).toBeFalsy();
+      }
+    );
+  });
+});
