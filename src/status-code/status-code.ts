6007f9b1683274c1706e08fd46dc93a8a24a2bca
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 19:12:51 -0400

    update status-code module to use new status code modules

--- src/status-code/status-code.ts
+++ src/status-code/status-code.ts
@@ -1,4 +1,8 @@
+import {InformationStatusCode} from "../information-status-code";
+import {SuccessfulStatusCode} from "../successful-status-code";
+
 /**
  * Indicate whether a specific HTTP request has been successfully completed.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#information_responses
  */
-export type StatusCode = number;
+export type StatusCode = InformationStatusCode | SuccessfulStatusCode;
