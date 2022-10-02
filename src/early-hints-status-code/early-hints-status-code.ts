4c4927369868a088182a5edec5b2b86302d72817
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 17:55:14 -0400

    create early-hints-status-code module

---
+++ src/early-hints-status-code/early-hints-status-code.ts
@@ -0,0 +1,11 @@
+/**
+ * The HTTP 103 Early Hints information response status code is primarily intended to be used with the Link header to allow the user agent to start preloading resources while the server is still preparing a response.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/103
+ */
+export type EarlyHintsStatusCode = 103;
+
+/**
+ * The HTTP 103 Early Hints information response status code is primarily intended to be used with the Link header to allow the user agent to start preloading resources while the server is still preparing a response.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/103
+ */
+export const EARLY_HINTS_STATUS_CODE: EarlyHintsStatusCode = 103;
---
+++ src/early-hints-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  EarlyHintsStatusCode,
+  EARLY_HINTS_STATUS_CODE,
+} from "./early-hints-status-code";
+export { isEarlyHintsStatusCode } from "./is-early-hints-status-code";
