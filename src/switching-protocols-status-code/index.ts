0cb0195957a881843189fd22fc4c2770f997690a
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 17:26:12 -0400

    create switching-protocols-status-code module

---
+++ src/switching-protocols-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  SwitchingProtocolsStatusCode,
+  SWITCHING_PROTOCOLS_STATUS_CODE,
+} from "./switching-protocols-status-code";
+export { isSwitchingProtocolsStatusCode } from "./is-switching-protocols-status-code";
---
+++ src/switching-protocols-status-code/switching-protocols-status-code.ts
@@ -0,0 +1,11 @@
+/**
+ * This code is sent in response to an Upgrade request header from the client and indicates the protocol the server is switching to.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/101
+ */
+export type SwitchingProtocolsStatusCode = 101;
+
+/**
+ * This code is sent in response to an Upgrade request header from the client and indicates the protocol the server is switching to.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/101
+ */
+export const SWITCHING_PROTOCOLS_STATUS_CODE: SwitchingProtocolsStatusCode = 101;
