7f049640a05b394a3ea8445fcb1c83e9d3072788
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-01 21:43:55 -0400

    add more valid and invalid example cases for is identified by

--- src/identified-by/is-identified-by.test.ts
+++ src/identified-by/is-identified-by.test.ts
@@ -3,9 +3,18 @@ import { isIdentifiedBy } from "./is-identified-by";
 const validExamples = [
   { id: "36955F34-2B2B-419B-918A-65357C55151F" },
   { id: "BEAB542D-5383-4C51-891E-7762D8D25A9B" },
+  { id: "059E1AF8-E478-49F8-8D17-AE85AFCC6130" },
+  { id: "B3C46B04-6AAF-452D-A79D-D210C54528B6" },
+  { id: "14DDB300-ED62-4CF6-A516-860EEE68A2D4" },
 ];
 
-const invalidExamples = [{ foo: "bar" }, { id: { foo: "bar" } }, { id: 0 }];
+const invalidExamples = [
+  { foo: "bar" },
+  { id: { foo: "bar" } },
+  { id: 0 },
+  { id: "14DDB300-ED62-4CF6-A516" },
+  { id: "918A-65357C55151F" },
+];
 
 describe("The isIdentifiedBy type-guard function", () => {
   describe("should return true", () => {
