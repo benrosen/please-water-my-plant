ed22bec430664190664d8de1ca1dce43fa161ed3
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 08:28:19 -0400

    add more valid and invalid examples for is identified by uuid tests

--- src/identified-by-uuid/is-identified-by-uuid.test.ts
+++ src/identified-by-uuid/is-identified-by-uuid.test.ts
@@ -1,8 +1,20 @@
 import { isIdentifiedByUuid } from "./is-identified-by-uuid";
 
-const validExamples = [{ id: "566132FE-331E-432B-9A95-9DC672735451" }];
+const validExamples = [
+  { id: "36955F34-2B2B-419B-918A-65357C55151F" },
+  { id: "BEAB542D-5383-4C51-891E-7762D8D25A9B" },
+  { id: "059E1AF8-E478-49F8-8D17-AE85AFCC6130" },
+  { id: "B3C46B04-6AAF-452D-A79D-D210C54528B6" },
+  { id: "14DDB300-ED62-4CF6-A516-860EEE68A2D4" },
+];
 
-const invalidExamples = [{ id: 0 }];
+const invalidExamples = [
+  { foo: "bar" },
+  { id: { foo: "bar" } },
+  { id: 0 },
+  { id: "14DDB300-ED62-4CF6-A516" },
+  { id: "918A-65357C55151F" },
+];
 
 describe("The isIdentifiedByUuid type-guard function", () => {
   describe("should return true", () => {
