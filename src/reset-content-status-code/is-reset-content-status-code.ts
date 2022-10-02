94bbfc38a681ec3c88e2e23e92c2c2fe05853a2e
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 18:47:00 -0400

    create reset-content-status-code module

---
+++ src/reset-content-status-code/is-reset-content-status-code.ts
@@ -0,0 +1,11 @@
+import {RESET_CONTENT_STATUS_CODE, ResetContentStatusCode,} from "./reset-content-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link ResetContentStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isResetContentStatusCode = (
+  value: unknown
+): value is ResetContentStatusCode => {
+  return typeof value === "number" && value === RESET_CONTENT_STATUS_CODE;
+};
