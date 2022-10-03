a85721c86d9a769903869d6d0639a10bddd430fd
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 20:47:17 -0400

    create moved-permanently-status-code module

---
+++ src/moved-permanently-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  MovedPermanentlyStatusCode,
+  MOVED_PERMANENTLY_STATUS_CODE,
+} from "./moved-permanently-status-code";
+export { isMovedPermanentlyStatusCode } from "./is-moved-permanently-status-code";
---
+++ src/moved-permanently-status-code/is-moved-permanently-status-code.ts
@@ -0,0 +1,11 @@
+import {MOVED_PERMANENTLY_STATUS_CODE, MovedPermanentlyStatusCode} from "./moved-permanently-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link MovedPermanentlyStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isMovedPermanentlyStatusCode = (
+  value: unknown
+): value is MovedPermanentlyStatusCode => {
+  return typeof value === "number" && value === MOVED_PERMANENTLY_STATUS_CODE;
+};
