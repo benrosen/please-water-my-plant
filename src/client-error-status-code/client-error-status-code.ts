281eaf4fc23828168dd958967dde7f90f3986215
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 19:14:00 -0400

    create client-error-status-code module

---
+++ src/client-error-status-code/client-error-status-code.ts
@@ -0,0 +1,13 @@
+import {BadRequestStatusCode} from "../bad-request-status-code";
+import {ForbiddenStatusCode} from "../forbidden-status-code";
+import {NotFoundStatusCode} from "../not-found-status-code";
+import {UnauthorizedStatusCode} from "../unauthorized-status-code";
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
+ */
+export type ClientErrorStatusCode =
+  | BadRequestStatusCode
+  | UnauthorizedStatusCode
+  | ForbiddenStatusCode
+  | NotFoundStatusCode;
