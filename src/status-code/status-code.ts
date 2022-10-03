fe4fb6b42f79bac2284f8123de2c5e74aa30dfbe
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 14:17:59 -0400

    extend status code type with redirection status codes

--- src/status-code/status-code.ts
+++ src/status-code/status-code.ts
@@ -1,8 +1,12 @@
 import {InformationStatusCode} from "../information-status-code";
+import {RedirectionStatusCode} from "../redirection-status-code";
 import {SuccessfulStatusCode} from "../successful-status-code";
 
 /**
  * Indicate whether a specific HTTP request has been successfully completed.
  * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
  */
-export type StatusCode = InformationStatusCode | SuccessfulStatusCode;
+export type StatusCode =
+  | InformationStatusCode
+  | SuccessfulStatusCode
+  | RedirectionStatusCode;
