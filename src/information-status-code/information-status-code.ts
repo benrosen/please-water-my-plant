348274a14781666f49e8f94b08510f1e2c99a9c8
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 14:18:22 -0400

    add links to status code documentation

--- src/information-status-code/information-status-code.ts
+++ src/information-status-code/information-status-code.ts
@@ -1,8 +1,11 @@
-import { ContinueStatusCode } from "../continue-status-code";
-import { EarlyHintsStatusCode } from "../early-hints-status-code";
-import { ProcessingStatusCode } from "../processing-status-code";
-import { SwitchingProtocolsStatusCode } from "../switching-protocols-status-code";
+import {ContinueStatusCode} from "../continue-status-code";
+import {EarlyHintsStatusCode} from "../early-hints-status-code";
+import {ProcessingStatusCode} from "../processing-status-code";
+import {SwitchingProtocolsStatusCode} from "../switching-protocols-status-code";
 
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#information_responses
+ */
 export type InformationStatusCode =
   | ContinueStatusCode
   | SwitchingProtocolsStatusCode
--- src/successful-status-code/successful-status-code.ts
+++ src/successful-status-code/successful-status-code.ts
@@ -1,11 +1,14 @@
-import { AcceptedStatusCode } from "../accepted-status-code";
-import { CreatedStatusCode } from "../created-status-code";
-import { NoContentStatusCode } from "../no-content-status-code";
-import { NonAuthoritativeInformationStatusCode } from "../non-authoritative-information-status-code";
-import { OkStatusCode } from "../ok-status-code";
-import { PartialContentStatusCode } from "../partial-content-status-code";
-import { ResetContentStatusCode } from "../reset-content-status-code";
+import {AcceptedStatusCode} from "../accepted-status-code";
+import {CreatedStatusCode} from "../created-status-code";
+import {NoContentStatusCode} from "../no-content-status-code";
+import {NonAuthoritativeInformationStatusCode} from "../non-authoritative-information-status-code";
+import {OkStatusCode} from "../ok-status-code";
+import {PartialContentStatusCode} from "../partial-content-status-code";
+import {ResetContentStatusCode} from "../reset-content-status-code";
 
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#successful_responses
+ */
 export type SuccessfulStatusCode =
   | OkStatusCode
   | CreatedStatusCode
