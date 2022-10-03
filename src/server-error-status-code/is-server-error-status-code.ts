d25f6847c25f60ec8a798ba821aaecbb7a1820c1
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 19:06:30 -0400

    create server-error-status-code module

---
+++ src/server-error-status-code/is-server-error-status-code.ts
@@ -0,0 +1,14 @@
+import {
+  isInternalServerErrorStatusCode
+} from "../internal-server-error-status-code/is-internal-server-error-status-code";
+import {ServerErrorStatusCode} from "./server-error-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link ServerErrorStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isServerErrorStatusCode = (
+  value: unknown
+): value is ServerErrorStatusCode => {
+  return isInternalServerErrorStatusCode(value);
+};
