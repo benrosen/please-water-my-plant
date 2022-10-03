97d165a7542158f7641dfeed528628d34f9a0aa6
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 20:34:05 -0400

    create found-status-code module

---
+++ src/found-status-code/is-found-status-code.ts
@@ -0,0 +1,9 @@
+import {FOUND_STATUS_CODE, FoundStatusCode} from "./found-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link FoundStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isFoundStatusCode = (value: unknown): value is FoundStatusCode => {
+  return typeof value === "number" && value === FOUND_STATUS_CODE;
+};
