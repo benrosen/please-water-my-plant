88be0f351a575151deec79420d65fe3bd2d43f34
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 13:30:02 -0400

    fix incorrect url in doc comment

--- src/status-code/status-code.ts
+++ src/status-code/status-code.ts
@@ -3,6 +3,6 @@ import {SuccessfulStatusCode} from "../successful-status-code";
 
 /**
  * Indicate whether a specific HTTP request has been successfully completed.
- * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#information_responses
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
  */
 export type StatusCode = InformationStatusCode | SuccessfulStatusCode;
