4baea095393cb7aa630d45bfb5a864b575d47345
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 21:07:03 -0400

    create temporary-redirect-status-code module

---
+++ src/temporary-redirect-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  TemporaryRedirectStatusCode,
+  TEMPORARY_REDIRECT_STATUS_CODE,
+} from "./temporary-redirect-status-code";
+export { isTemporaryRedirectStatusCode } from "./is-temporary-redirect-status-code";
---
+++ src/temporary-redirect-status-code/temporary-redirect-status-code.test.ts
@@ -0,0 +1,28 @@
+import { isTemporaryRedirectStatusCode } from "./is-temporary-redirect-status-code";
+import { TEMPORARY_REDIRECT_STATUS_CODE } from "./temporary-redirect-status-code";
+
+const validExamples = [TEMPORARY_REDIRECT_STATUS_CODE];
+
+const invalidExamples = [0, 1, 101, 1001, 200, 404];
+
+describe("The isTemporaryRedirectStatusCode type-guard function", () => {
+  describe("should return true", () => {
+    test.each(validExamples)(
+      "when the given value, %j, implements the TemporaryRedirectStatusCode interface",
+      (validExample) => {
+        const result = isTemporaryRedirectStatusCode(validExample);
+        expect(result).toBeTruthy();
+      }
+    );
+  });
+
+  describe("should return false", () => {
+    test.each(invalidExamples)(
+      "when the given value, %j, does not implement the TemporaryRedirectStatusCode interface",
+      (invalidExample) => {
+        const result = isTemporaryRedirectStatusCode(invalidExample);
+        expect(result).toBeFalsy();
+      }
+    );
+  });
+});
