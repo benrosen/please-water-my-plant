f64d435183b47693a32c6eb856d03f131b28f06f
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 15:17:31 -0400

    create bad-request-status-code

---
+++ src/bad-request-status-code/bad-request-status-code.test.ts
@@ -0,0 +1,28 @@
+import { BAD_REQUEST_STATUS_CODE } from "./bad-request-status-code";
+import { isBadRequestStatusCode } from "./is-bad-request-status-code";
+
+const validExamples = [BAD_REQUEST_STATUS_CODE];
+
+const invalidExamples = [0, 1, 101, 1001, 200, 404];
+
+describe("The isBadRequestStatusCode type-guard function", () => {
+  describe("should return true", () => {
+    test.each(validExamples)(
+      "when the given value, %j, implements the BadRequestStatusCode interface",
+      (validExample) => {
+        const result = isBadRequestStatusCode(validExample);
+        expect(result).toBeTruthy();
+      }
+    );
+  });
+
+  describe("should return false", () => {
+    test.each(invalidExamples)(
+      "when the given value, %j, does not implement the BadRequestStatusCode interface",
+      (invalidExample) => {
+        const result = isBadRequestStatusCode(invalidExample);
+        expect(result).toBeFalsy();
+      }
+    );
+  });
+});
