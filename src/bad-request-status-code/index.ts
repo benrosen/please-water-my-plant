3cbbe365ebd0595c80d2b43ccb18a22304750456
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 15:17:31 -0400

    create bad-request-status-code

---
+++ src/bad-request-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  BadRequestStatusCode,
+  BAD_REQUEST_STATUS_CODE,
+} from "./bad-request-status-code";
+export { isBadRequestStatusCode } from "./is-bad-request-status-code";
---
+++ src/bad-request-status-code/is-bad-request-status-code.ts
@@ -0,0 +1,11 @@
+import {BAD_REQUEST_STATUS_CODE, BadRequestStatusCode} from "./bad-request-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link BadRequestStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isBadRequestStatusCode = (
+  value: unknown
+): value is BadRequestStatusCode => {
+  return typeof value === "number" && value === BAD_REQUEST_STATUS_CODE;
+};
