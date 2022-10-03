ab33edb7ce477fe99fb229c54d4b49d46c426e2a
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 17:12:35 -0400

    explicitly type bad request status code

--- src/bad-request-status-code/bad-request-status-code.ts
+++ src/bad-request-status-code/bad-request-status-code.ts
@@ -8,4 +8,4 @@ export type BadRequestStatusCode = 400;
  * The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).
  * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400
  */
-export const BAD_REQUEST_STATUS_CODE = 400;
+export const BAD_REQUEST_STATUS_CODE: BadRequestStatusCode = 400;
