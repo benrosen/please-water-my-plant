e9ac22f9792f628ad1325bb87eac2b8d6996f9b6
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 20:40:58 -0400

    create multiple-choice-status-code module

---
+++ src/multiple-choices-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  MultipleChoicesStatusCode,
+  MULTIPLE_CHOICE_STATUS_CODE,
+} from "./multiple-choices-status-code";
+export { isMultipleChoicesStatusCode } from "./is-multiple-choices-status-code";
