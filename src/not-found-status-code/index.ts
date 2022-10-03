afb5b3b1ac306b0ebf9cd9e7e7494987f89df6fd
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-03 19:01:01 -0400

    create not-found-status-code module

---
+++ src/not-found-status-code/index.ts
@@ -0,0 +1,5 @@
+export {
+  NotFoundStatusCode,
+  NOT_FOUND_STATUS_CODE,
+} from "./not-found-status-code";
+export { isNotFoundStatusCode } from "./is-not-found-status-code";
---
+++ src/not-found-status-code/is-not-found-status-code.ts
@@ -0,0 +1,11 @@
+import {NOT_FOUND_STATUS_CODE, NotFoundStatusCode} from "./not-found-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link NotFoundStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isNotFoundStatusCode = (
+  value: unknown
+): value is NotFoundStatusCode => {
+  return typeof value === "number" && value === NOT_FOUND_STATUS_CODE;
+};
