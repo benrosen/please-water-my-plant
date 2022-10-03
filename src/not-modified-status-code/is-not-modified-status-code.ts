911e93b385df20a187ebecbeccce07771265d96d
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-02 20:59:28 -0400

    create not-modified-status-code module

---
+++ src/not-modified-status-code/is-not-modified-status-code.ts
@@ -0,0 +1,11 @@
+import {NOT_MODIFIED_STATUS_CODE, NotModifiedStatusCode,} from "./not-modified-status-code";
+
+/**
+ * Return `true` if the given value (of `unknown` type) is of type {@link NotModifiedStatusCode}. Otherwise, return `false`.
+ * @param value A value of `unknown` type.
+ */
+export const isNotModifiedStatusCode = (
+  value: unknown
+): value is NotModifiedStatusCode => {
+  return typeof value === "number" && value === NOT_MODIFIED_STATUS_CODE;
+};
