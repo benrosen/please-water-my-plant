36b981ac336c0443ac9cbd57c89d7851e40fbaa4
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-04 16:37:36 -0400

    further constrain timestamp with PositiveNumber and Integer types

--- src/timestamp/timestamp.ts
+++ src/timestamp/timestamp.ts
@@ -1,6 +1,8 @@
+import {Integer} from "../integer";
 import {Milliseconds} from "../milliseconds";
+import {PositiveNumber} from "../positive-number";
 
 /**
  * Represents the number of {@link Milliseconds} since the beginning of the Unix Epoch.
  */
-export type Timestamp = Milliseconds;
+export type Timestamp = Milliseconds & PositiveNumber & Integer;
