5701a1d6f9886da18a751b64e62f211d2c79d6a5
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-06 13:46:30 -0400

    add more test cases for is vector

--- src/vector/is-vector.test.ts
+++ src/vector/is-vector.test.ts
@@ -1,8 +1,20 @@
 import { isVector } from "../vector";
 
-const validExamples = [{ x: 0, y: 0 }];
+const validExamples = [
+  { x: 0, y: 0 },
+  { x: 0, y: -1 },
+  { x: -1, y: 0 },
+  { x: 1, y: -1 },
+  { x: 0.090087, y: 1.324 },
+];
 
-const invalidExamples = [{ entity: "foo" }, { id: "bar" }];
+const invalidExamples = [
+  { entity: "foo" },
+  { id: "bar" },
+  { x: 0, y: false },
+  { x: "baz", y: 0 },
+  { x: 0, z: 0 },
+];
 
 describe("The isVector type-guard function", () => {
   describe("should return true", () => {
