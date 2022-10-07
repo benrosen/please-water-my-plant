a733ef804ee0dc2698501fc3086c8263fbc16444
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-07 11:08:28 -0400

    further constrain is component index type guard

--- src/component-index/is-component-index.ts
+++ src/component-index/is-component-index.ts
@@ -1,3 +1,5 @@
+import {isComponent} from "../component";
+import {isUuid} from "../uuid";
 import {ComponentIndex} from "./component-index";
 
 /**
@@ -5,8 +7,37 @@ import {ComponentIndex} from "./component-index";
  * @param value A value of `unknown` type.
  */
 export const isComponentIndex = (value: unknown): value is ComponentIndex => {
-  const maybeComponentIndex = value as ComponentIndex;
-  return (
-    !!maybeComponentIndex.byComponentId && !!maybeComponentIndex.byEntityId
-  );
+  try {
+    const maybeComponentIndex = value as ComponentIndex;
+
+    const hasComponentsIndexedByComponentId = Object.entries(
+        maybeComponentIndex.byComponentId
+    ).every(([key, value]) => {
+      return isUuid(key) && isComponent(value);
+    });
+
+    if (!hasComponentsIndexedByComponentId) {
+      return false;
+    }
+
+    const hasComponentIdsIndexedByEntityId = Object.entries(
+        maybeComponentIndex.byEntityId
+    ).every(([key, value]) => {
+      return isUuid(key) && Array.isArray(value) && value.every(isUuid);
+    });
+
+    if (!hasComponentIdsIndexedByEntityId) {
+      return false;
+    }
+
+    if (maybeComponentIndex.custom) {
+      return Object.entries(maybeComponentIndex.custom).every(([key, value]) => {
+        return typeof key === "string" && value.every(isUuid);
+      })
+    } else {
+      return true
+    }
+  } catch {
+    return false;
+  }
 };
