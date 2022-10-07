843f44a119266b567221a9223c08389bfb334937
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-06 20:50:33 -0400

    remove fixed point decimal places const in favor of importing the value from package.json

--- src/fix-float/fix-float.ts
+++ src/fix-float/fix-float.ts
@@ -1,4 +1,4 @@
-import {FIXED_POINT_DECIMAL_PLACES} from "../fixed-point-decimal-places";
+import {config} from "../../package.json";
 
 /**
  * Convert a floating-point number to a fixed-point number.
@@ -6,5 +6,5 @@ import {FIXED_POINT_DECIMAL_PLACES} from "../fixed-point-decimal-places";
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
  */
 export const fixFloat = (float: number): number => {
-  return +float.toFixed(FIXED_POINT_DECIMAL_PLACES);
+  return +float.toFixed(config.fixedPointDecimalPlaces);
 };
