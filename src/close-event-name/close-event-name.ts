21b9ce06012e5dcf5086d34fcbb1f27be0eef571
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-07 20:53:28 -0400

    add link to documentation for close event name

--- src/close-event-name/close-event-name.ts
+++ src/close-event-name/close-event-name.ts
@@ -1,4 +1,5 @@
 /**
  * Indicates that the request is completed, or its underlying connection was terminated prematurely (before the response completion).
+ * @see https://nodejs.org/api/http.html#event-close
  */
 export const CloseEventName = "close";
