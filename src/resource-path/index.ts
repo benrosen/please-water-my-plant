ebe023739b82c1ea01ae454601b9f9b4cce702fe
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-07 22:21:37 -0400

    create resource path module

---
+++ src/resource-path/index.ts
@@ -0,0 +1,2 @@
+export { ResourcePath } from "./resource-path";
+export { isResourcePath } from "./is-resource-path";
---
+++ src/resource-path/resource-path.ts
@@ -0,0 +1,3 @@
+import { StartsWith } from "../starts-with";
+
+export type ResourcePath = StartsWith<"/">;
