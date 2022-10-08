0503e6463ab768423b40b40b3800fb3c0c4ba2e9
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-08 19:21:27 -0400

    create protagonist page

--- src/protagonist-page/protagonist-page.ts
+++ src/protagonist-page/protagonist-page.ts
@@ -3,9 +3,22 @@ export const ProtagonistPage = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <title>Monster Mashup</title>
+    <style>
+        canvas {
+            outline: 1px solid black;
+        }
+    </style>
 </head>
 <body>
-<h1>Protagonist Page</h1>
-<p>Hello world!</p>
+    <h1>Protagonist Page</h1>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/antagonist">Switch sides</a>
+        </li>
+    </ul>
+    <canvas></canvas>
 </body>
 </html>`;
