6e3b96d77d78849b067cc97a12eaf33bb9ce9b2e
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-07 20:19:00 -0400

    add more test cases for create-component-index.test.ts

--- src/create-component-index/create-component-index.test.ts
+++ src/create-component-index/create-component-index.test.ts
@@ -1,13 +1,80 @@
+import { isAffectedByGravity } from "../affected-by-gravity";
+import { Component } from "../component";
+import { ComponentIndex } from "../component-index";
+import { ComponentIndexer } from "../component-indexer";
 import { createComponentIndex } from "./create-component-index";
 
 describe("The createComponentIndex function", () => {
-  it("should return the expected result", () => {
-    const components = [];
-    const expectedComponentIndex = {
-      byComponentId: {},
-      byEntityId: {},
-    };
-    const result = createComponentIndex({ components });
-    expect(result).toStrictEqual(expectedComponentIndex);
-  });
+  test.each([
+    [
+      [[], []],
+      {
+        byComponentId: {},
+        byEntityId: {},
+      },
+    ],
+    [
+      [
+        [
+          {
+            id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+            entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
+          },
+        ],
+        [],
+      ],
+      {
+        byComponentId: {
+          ["7d1e8ee0-3310-44db-9c8c-9a9556a30bca"]: {
+            id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+            entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
+          },
+        },
+        byEntityId: {
+          ["2c2a0e29-98c5-43b1-a452-91eca64a8ba8"]: [
+            "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+          ],
+        },
+      },
+    ],
+    [
+      [
+        [
+          {
+            id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+            entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
+            affectedByGravity: true,
+          },
+        ],
+        [isAffectedByGravity],
+      ],
+      {
+        byComponentId: {
+          ["7d1e8ee0-3310-44db-9c8c-9a9556a30bca"]: {
+            id: "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+            entityId: "2c2a0e29-98c5-43b1-a452-91eca64a8ba8",
+            affectedByGravity: true,
+          },
+        },
+        byEntityId: {
+          ["2c2a0e29-98c5-43b1-a452-91eca64a8ba8"]: [
+            "7d1e8ee0-3310-44db-9c8c-9a9556a30bca",
+          ],
+        },
+        custom: {
+          isAffectedByGravity: ["7d1e8ee0-3310-44db-9c8c-9a9556a30bca"],
+        },
+      },
+    ],
+  ] as [[Component[], ComponentIndexer[]], ComponentIndex][])(
+    "should return the expected result",
+    ([components, indexers], expectedComponentIndex) => {
+      const result = createComponentIndex({
+        components,
+        indexers,
+      });
+
+      expect(result).toStrictEqual(expectedComponentIndex);
+    }
+  );
 });
