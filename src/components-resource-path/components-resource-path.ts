322006bcee7b28bfd7f201f771ac291f849d1f64
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-08 13:15:59 -0400

    document path constants

--- src/components-resource-path/components-resource-path.ts
+++ src/components-resource-path/components-resource-path.ts
@@ -1,3 +1,6 @@
-import { ResourcePath } from "resource-path";
+import {ResourcePath} from "resource-path";
 
+/**
+ * A {@link ResourcePath} indicating the location of the {@link Component} resource.
+ */
 export const ComponentsResourcePath: ResourcePath = "/components";
