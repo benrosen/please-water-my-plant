8e2dae8ce4dbde2c1d1051901408d7e02d8ea3d1
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-08 11:30:13 -0400

    create not found page module

---
+++ src/not-found-page/not-found-page.ts
@@ -0,0 +1,11 @@
+export const NotFoundPage = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Monster Mashup</title>
+</head>
+<body>
+<h1>Not Found</h1>
+<p>Hello world!</p>
+</body>
+</html>`;
