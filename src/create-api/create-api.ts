b41ead77ef708d8bc6b1ff74267478b2cc6b0521
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-10 21:02:35 -0400

    send html index files instead of strings

--- src/create-api/create-api.ts
+++ src/create-api/create-api.ts
@@ -4,20 +4,17 @@ import {Component} from "component";
 import {ComponentsResourcePath} from "components-resource-path";
 import * as cors from "cors";
 import * as express from "express";
-import {HomePage} from "home-page";
 import {HomePagePath} from "home-page-path";
 import {OK_STATUS_CODE} from "ok-status-code";
 import {Order} from "order";
 import {OrderResourcePath} from "order-resource-path";
 import {parseOrderFromRequest} from "parse-order-from-request";
+import * as path from "path";
 import {respondWithAccepted} from "respond-with-accepted";
 import {respondWithBadRequestError} from "respond-with-bad-request-error";
 import {respondWithInternalServerError} from "respond-with-internal-server-error";
 import {ServerSentEventHeaders} from "server-sent-event-headers";
-import {AntagonistPage} from "../antagonist-page";
 import {AntagonistPagePath} from "../antagonist-page-path";
-import {NotFoundPage} from "../not-found-page";
-import {ProtagonistPage} from "../protagonist-page";
 import {ProtagonistPagePath} from "../protagonist-page-path";
 
 /**
@@ -43,16 +40,18 @@ export const createApi = ({
 
   server.use(cors());
 
+  server.use(express.static(path.join(__dirname, "antagonist-page")));
+
   server.get(HomePagePath, (request, response) => {
-    response.send(HomePage);
+    response.sendFile(path.join(__dirname, "../home-page/index.html"));
   });
 
   server.get(AntagonistPagePath, (request, response) => {
-    response.send(AntagonistPage);
+    response.sendFile(path.join(__dirname, "../antagonist-page/index.html"));
   });
 
   server.get(ProtagonistPagePath, (request, response) => {
-    response.send(ProtagonistPage);
+    response.sendFile(path.join(__dirname, "../protagonist-page/index.html"));
   });
 
   server.get(ComponentsResourcePath, (request, response) => {
@@ -84,22 +83,22 @@ export const createApi = ({
     }
   });
 
-  server.use((req, res) => {
-    res.status(404);
+  server.use((request, response) => {
+    response.status(404);
 
-    if (req.accepts("html")) {
-      res.send(NotFoundPage);
-
-      return;
-    }
-
-    if (req.accepts("json")) {
-      res.json({ error: "Not found" });
+    if (request.accepts("html")) {
+      response.sendFile(path.join(__dirname, "../not-found-page/index.html"));
 
       return;
     }
 
-    res.type("txt").send("Not found");
+    if (request.accepts("json")) {
+      response.json({ error: "Not found" });
+
+      return;
+    }
+
+    response.type("txt").send("Not found");
   });
 
   return server;
