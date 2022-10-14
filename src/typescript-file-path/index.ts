5c565515f415e83be94c46ca8b1de38e91614d68
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-13 21:46:14 -0400

    create typescript-file-path module

---
+++ src/typescript-file-path/index.ts
@@ -0,0 +1,2 @@
+export { TypescriptFilePath } from "./typescript-file-path";
+export { isTypescriptFilePath } from "./is-typescript-file-path";
---
+++ src/typescript-file-path/typescript-file-path.ts
@@ -0,0 +1,7 @@
+import {EndsWith} from "../ends-with";
+import {TypescriptFileExtension} from "../typescript-file-extension";
+
+/**
+ * A string that {@link EndsWith} a {@link TypescriptFileExtension}.
+ */
+export type TypescriptFilePath = EndsWith<TypescriptFileExtension>;
