8a63f74631699a4835e1fa377fe638a9a21f9823
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-13 22:06:44 -0400

    fix typescript file path import paths

--- src/typescript-file-path/typescript-file-path.ts
+++ src/typescript-file-path/typescript-file-path.ts
@@ -1,5 +1,5 @@
-import {EndsWith} from "../ends-with";
-import {TypescriptFileExtension} from "../typescript-file-extension";
+import {EndsWith} from "ends-with";
+import {TypescriptFileExtension} from "typescript-file-extension";
 
 /**
  * A string that {@link EndsWith} a {@link TypescriptFileExtension}.
