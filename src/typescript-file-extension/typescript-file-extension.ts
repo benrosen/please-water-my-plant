c887734726b9b05c0ab9bffce832f29256229fa9
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-13 22:07:08 -0400

    link to file extension module from typescript-file-extension.ts

--- src/typescript-file-extension/typescript-file-extension.ts
+++ src/typescript-file-extension/typescript-file-extension.ts
@@ -1,9 +1,9 @@
 /**
- * The file extension for TypeScript files.
+ * The {@link FileExtension} for TypeScript files.
  */
 export type TypescriptFileExtension = ".ts";
 
 /**
- * The file extension for TypeScript files.
+ * The {@link FileExtension} for TypeScript files.
  */
 export const TYPESCRIPT_FILE_EXTENSION: TypescriptFileExtension = ".ts";
