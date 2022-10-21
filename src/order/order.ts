0bb8d5d3f5f20d0905e61bc50923861703421538
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-21 00:05:38 -0400

    enhance documentation for order interface

--- src/order/order.ts
+++ src/order/order.ts
@@ -3,5 +3,6 @@ import {Timestamped} from "timestamped";
 
 /**
  * An {@link Order} is a {@link Timestamped} {@link Component}.
+ * The `entityId` of an {@link Order} references the client that created the {@link Order}.
  */
 export interface Order extends Component, Timestamped {}
