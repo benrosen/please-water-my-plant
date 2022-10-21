cfccf362199a058654b4d9d2622c6049a40f5e92
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-21 00:14:03 -0400

    call createConsole with id parameter

--- src/create-console/create-console.test.ts
+++ src/create-console/create-console.test.ts
@@ -1,6 +1,7 @@
 /** @jest-environment jsdom */
 
 import { Game } from "phaser";
+import { createUuid } from "../uuid";
 import { createConsole } from "./create-console";
 
 jest.mock("phaser", () => {
@@ -13,8 +14,11 @@ const mockedGame = jest.mocked(Game);
 
 describe("The createConsole function", () => {
   test("creates a new Game instance", () => {
+    const id = createUuid()
+
     createConsole({
       createController: jest.fn(),
+      id
     });
 
     expect(mockedGame).toHaveBeenCalled();
