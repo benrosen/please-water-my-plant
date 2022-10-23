f84faa407161a2c27560ae570e00630892aa70ee
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-23 16:28:33 -0400

    create antagonistic module

---
+++ src/antagonistic/antagonistic.ts
@@ -0,0 +1,4 @@
+import { Antagonist } from "antagonist";
+import { RoleRelated } from "role-related";
+
+export interface Antagonistic extends RoleRelated<Antagonist> {}
---
+++ src/antagonistic/index.ts
@@ -0,0 +1,2 @@
+export { Antagonistic } from "./antagonistic";
+export { isAntagonistic } from "./is-antagonistic";
