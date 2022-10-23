2e483c4689bb8da9639abf9d526cacff3853f170
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-23 15:48:04 -0400

    modify order to accept generic string name

--- src/order/order.ts
+++ src/order/order.ts
@@ -5,4 +5,8 @@ import {Timestamped} from "timestamped";
  * An {@link Order} is a {@link Timestamped} {@link Component}.
  * The `entityId` of an {@link Order} references the client that created the {@link Order}.
  */
-export interface Order extends Component, Timestamped {}
+export interface Order<GenericString extends string>
+  extends Component,
+    Timestamped {
+  name: GenericString;
+}
