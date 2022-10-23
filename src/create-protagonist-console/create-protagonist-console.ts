60ac43d4546bca859c5b9df4d1f85d8074825c0a
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-23 15:47:11 -0400

    explicitly define role when creating protagonist console

--- src/create-protagonist-console/create-protagonist-console.ts
+++ src/create-protagonist-console/create-protagonist-console.ts
@@ -1,6 +1,7 @@
 import {createConsole} from "create-console";
 import {createProtagonistController} from "create-protagonist-controller";
 import {IdentifiedByUuid} from "identified-by-uuid";
+import {PROTAGONIST} from "protagonist";
 
 /**
  * Create a {@link Game} instance that can render a {@link Component} array and post {@link Order} requests in response to protagonist input.
@@ -11,5 +12,6 @@ export const createProtagonistConsole = ({ id }: IdentifiedByUuid) => {
   createConsole({
     createController: createProtagonistController,
     id,
+    role: PROTAGONIST,
   });
 };
