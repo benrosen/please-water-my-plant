8caa97f50e536ad6237c2d56ceababa77970afa9
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-24 11:53:27 -0400

    use named interface; set default generic value

--- src/order/order.ts
+++ src/order/order.ts
@@ -1,12 +1,13 @@
 import {Component} from "component";
+import {Name} from "name";
+import {Named} from "named";
 import {Timestamped} from "timestamped";
 
 /**
  * An {@link Order} is a {@link Timestamped} {@link Component}.
  * The `entityId` of an {@link Order} references the client that created the {@link Order}.
  */
-export interface Order<GenericString extends string>
+export interface Order<GenericName extends Name = Name>
   extends Component,
-    Timestamped {
-  name: GenericString;
-}
+    Named<GenericName>,
+    Timestamped {}
