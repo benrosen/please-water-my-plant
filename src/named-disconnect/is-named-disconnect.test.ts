a753daad7cd8cb97481ccfd465bb1af297365469
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-24 14:41:06 -0400

    create named disconnect module

---
+++ src/named-disconnect/is-named-disconnect.test.ts
@@ -0,0 +1,27 @@
+import { isNamedDisconnect } from "./is-named-disconnect";
+
+const validExamples = [];
+
+const invalidExamples = [];
+
+describe("The isNamedDisconnect type-guard function", () => {
+  describe("should return true", () => {
+    test.each(validExamples)(
+      "when the given value, %j, implements the NamedDisconnect interface",
+      (validExample) => {
+        const result = isNamedDisconnect(validExample);
+        expect(result).toBeTruthy();
+      }
+    );
+  });
+
+  describe("should return false", () => {
+    test.each(invalidExamples)(
+      "when the given value, %j, does not implement the NamedDisconnect interface",
+      (invalidExample) => {
+        const result = isNamedDisconnect(invalidExample);
+        expect(result).toBeFalsy();
+      }
+    );
+  });
+});
