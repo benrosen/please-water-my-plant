d11d4db3740357473d5be8570570e46984f1023c
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-24 22:20:22 -0400

    add connect and disconnect order indexers

--- src/get-indexers/get-indexers.ts
+++ src/get-indexers/get-indexers.ts
@@ -1,9 +1,11 @@
 import {ComponentIndexer} from "component-indexer";
+import {isConnectOrder} from "connect-order";
+import {isDisconnectOrder} from "disconnect-order";
 import {isOrder} from "order";
 
 /**
  * Return an array of {@link ComponentIndexer} functions.
  */
 export const getIndexers = (): ComponentIndexer[] => {
-  return [isOrder];
+  return [isConnectOrder, isDisconnectOrder, isOrder];
 };
