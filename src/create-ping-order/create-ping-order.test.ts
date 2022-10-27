0bc5a53bcad65cced24f156981f20e0a7cf7bf98
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-26 21:00:59 -0400

    create create-ping-order module

---
+++ src/create-ping-order/create-ping-order.test.ts
@@ -0,0 +1,27 @@
+import { PING } from "../ping";
+import { createPingOrder } from "./create-ping-order";
+
+describe("The createPingOrder function", () => {
+  test.each([
+    [
+      {
+        entityId: "409e5725-8cfb-4238-b52f-119317a1747f",
+        position: { x: 0, y: 0 },
+      },
+      {
+        id: expect.anything(),
+        position: { x: 0, y: 0 },
+        entityId: "409e5725-8cfb-4238-b52f-119317a1747f",
+        name: PING,
+        timestamp: expect.anything(),
+      },
+    ],
+  ] as [Parameters<typeof createPingOrder>[0], ReturnType<typeof createPingOrder>][])(
+    "should return the expected result.",
+    (validExample, expectedResult) => {
+      const result = createPingOrder(validExample);
+
+      expect(result).toStrictEqual(expectedResult);
+    }
+  );
+});
