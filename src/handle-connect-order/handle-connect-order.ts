49b43764b9c63fbb00c8e77c9c2ba4650d609de3
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-30 09:34:11 -0400

    document connect and disconnect order handlers

---
+++ src/handle-connect-order/handle-connect-order.ts
@@ -0,0 +1,36 @@
+import {isAntagonist} from "antagonist";
+import {ComponentIndex} from "component-index";
+import {ConnectOrder} from "connect-order";
+import {createAntagonisticComponent} from "create-antagonistic-component";
+import {createProtagonisticComponent} from "create-protagonistic-component";
+import {isProtagonist} from "protagonist";
+
+/**
+ * Handle a {@link ConnectOrder}
+ * @param params The config options
+ * @param params.componentIndex The {@link ComponentIndex} that contains the {@link ConnectOrder}
+ * @param params.order The {@link ConnectOrder} to handle
+ */
+export const handleConnectOrder = ({
+  componentIndex,
+  order,
+}: {
+  componentIndex: ComponentIndex;
+  order: ConnectOrder;
+}) => {
+  if (isAntagonist(order.role)) {
+    return createAntagonisticComponent({
+      componentIndex,
+      entityId: order.entityId,
+    });
+  }
+
+  if (isProtagonist(order.role)) {
+    return createProtagonisticComponent({
+      componentIndex,
+      entityId: order.entityId,
+    });
+  }
+
+  throw new Error();
+};
---
+++ src/handle-disconnect-orders/handle-disconnect-orders.ts
@@ -0,0 +1,26 @@
+import {ComponentIndex} from "component-index";
+import {getDisconnectOrders} from "get-disconnect-orders";
+import {handleDisconnectOrder} from "handle-disconnect-order";
+
+/**
+ * Handle every {@link DisconnectOrder} in the given {@link ComponentIndex}
+ * @param params The config options
+ * @param params.componentIndex The {@link ComponentIndex} that contains the {@link DisconnectOrder}
+ */
+export const handleDisconnectOrders = ({
+  componentIndex,
+}: {
+  componentIndex: ComponentIndex;
+}) => {
+  const disconnectOrders = getDisconnectOrders({ componentIndex });
+
+  return disconnectOrders.reduce<ComponentIndex>(
+    (reducedComponentIndex, disconnectOrder) => {
+      return handleDisconnectOrder({
+        componentIndex: reducedComponentIndex,
+        order: disconnectOrder,
+      });
+    },
+    componentIndex
+  );
+};
