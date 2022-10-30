2fe89845c2ec8b0012810c277bcb63b926b4cf58
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-30 09:40:13 -0400

    document get connect orders and get disconnect orders

---
+++ src/get-connect-orders/get-connect-orders.ts
@@ -0,0 +1,19 @@
+import {ComponentIndex} from "component-index";
+import {ConnectOrder, isConnectOrder} from "connect-order";
+import {getComponentsByCustomIndex} from "get-components-by-custom-index";
+
+/**
+ * Retrieve an array of {@link ConnectOrder} instances.
+ * @param params The config options
+ * @param params.componentIndex The {@link ComponentIndex} that contains the {@link ConnectOrder} instances.
+ */
+export const getConnectOrders = ({
+  componentIndex,
+}: {
+  componentIndex: ComponentIndex;
+}) => {
+  return getComponentsByCustomIndex<ConnectOrder>({
+    componentIndex,
+    indexName: isConnectOrder.name,
+  });
+};
---
+++ src/get-disconnect-orders/get-disconnect-orders.ts
@@ -0,0 +1,19 @@
+import {ComponentIndex} from "component-index";
+import {DisconnectOrder, isDisconnectOrder} from "disconnect-order";
+import {getComponentsByCustomIndex} from "get-components-by-custom-index";
+
+/**
+ * Retrieve an array of {@link DisconnectOrder} instances.
+ * @param params The config options
+ * @param params.componentIndex The {@link ComponentIndex} that contains the {@link DisconnectOrder} instances.
+ */
+export const getDisconnectOrders = ({
+  componentIndex,
+}: {
+  componentIndex: ComponentIndex;
+}) => {
+  return getComponentsByCustomIndex<DisconnectOrder>({
+    componentIndex,
+    indexName: isDisconnectOrder.name,
+  });
+};
