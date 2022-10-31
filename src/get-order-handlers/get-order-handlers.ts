438d4cefa11c10384244a8224e6ded78a93c3eaf
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-30 22:39:20 -0400

    further constrain return type of get order handlers function

--- src/get-order-handlers/get-order-handlers.ts
+++ src/get-order-handlers/get-order-handlers.ts
@@ -1,9 +1,12 @@
 import {handleConnectOrders} from "handle-connect-orders";
 import {handleDisconnectOrders} from "handle-disconnect-orders";
+import {ComponentIndex} from "../component-index";
 
 /**
  * Return an array of functions that can handle {@link Order} instances
  */
-export const getOrderHandlers = () => {
+export const getOrderHandlers = (): (({
+  componentIndex: ComponentIndex,
+}) => ComponentIndex)[] => {
   return [handleConnectOrders, handleDisconnectOrders];
 };
