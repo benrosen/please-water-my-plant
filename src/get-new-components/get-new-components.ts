dd4c36335b24204f9c6d611208f3c9d99433dd76
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-30 21:31:37 -0400

    handle orders as part of getting new components

--- src/get-new-components/get-new-components.ts
+++ src/get-new-components/get-new-components.ts
@@ -1,12 +1,17 @@
 import {Component} from "component";
 import {ComponentIndex} from "component-index";
+import {handleOrders} from "handle-orders";
 
 /**
  * Derive a {@link Component} array representing next state of the game from a {@link ComponentIndex} representing the current state of the game.
  * @param componentIndex The {@link ComponentIndex} representing the current state of the game.
  */
-export const getNewComponents = ({}: {
+export const getNewComponents = ({
+  componentIndex,
+}: {
   componentIndex: ComponentIndex;
 }): Component[] => {
-  return [];
+  componentIndex = handleOrders({ componentIndex });
+
+  return Object.values(componentIndex.byComponentId);
 };
