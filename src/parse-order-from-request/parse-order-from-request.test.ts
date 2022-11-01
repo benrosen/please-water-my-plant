71520aace62570d923546f5375c64d9c07f8607f
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-10-31 20:30:57 -0400

    test parse-order-from-request module

--- src/parse-order-from-request/parse-order-from-request.test.ts
+++ src/parse-order-from-request/parse-order-from-request.test.ts
@@ -1,35 +1,29 @@
+import { CONNECT } from "connect";
 import { parseOrderFromRequest } from "parse-order-from-request";
 
 describe("The parseOrderFromRequest function", () => {
   test.each([
     [
       {
-        request: {
-          body: {
-            id: "2515934A-F479-4785-8760-62366BD5CB13",
-            entityId: "213C4A66-8C0F-4CDA-A684-3FD967A5DF5A",
-            timestamp: 0,
-          },
+        body: {
+          id: "2515934A-F479-4785-8760-62366BD5CB13",
+          entityId: "213C4A66-8C0F-4CDA-A684-3FD967A5DF5A",
+          timestamp: 0,
+          name: CONNECT,
         },
       },
       {
         id: "2515934A-F479-4785-8760-62366BD5CB13",
         entityId: "213C4A66-8C0F-4CDA-A684-3FD967A5DF5A",
         timestamp: 0,
+        name: CONNECT,
       },
-    ],
-    [
-      {
-        request: {
-          body: {},
-        },
-      },
-      undefined,
     ],
   ] as [Parameters<typeof parseOrderFromRequest>[0], ReturnType<typeof parseOrderFromRequest>][])(
     "should return the expected result.",
     (validExample, expectedResult) => {
       const result = parseOrderFromRequest(validExample);
+
       expect(result).toStrictEqual(expectedResult);
     }
   );
--- src/parse-order-from-request/parse-order-from-request.ts
+++ src/parse-order-from-request/parse-order-from-request.ts
@@ -4,17 +4,15 @@ import {isOrder, Order} from "order";
 /**
  * Parse an {@link Order} instance from an incoming request.
  * @param params The config options
- * @param params.request The request from which to parse the {@link Order} instance.
+ * @param params.body The request body from which to parse the {@link Order} instance.
  */
 export const parseOrderFromRequest = ({
-  request,
-}: {
-  request: Request;
-}): Order | undefined => {
-  const canParseOrder = isOrder(request.body);
+  body,
+}: Pick<Request, "body">): Order | undefined => {
+  const canParseOrder = isOrder(body);
 
   if (canParseOrder) {
-    return request.body;
+    return body;
   } else {
     return undefined;
   }
