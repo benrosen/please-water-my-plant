d107fef3f2f7a8008d4dc85e02f598b44e6674a5
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-03 18:45:21 -0400

    add test cases

--- src/create-connect-order/create-connect-order.test.ts
+++ src/create-connect-order/create-connect-order.test.ts
@@ -1,13 +1,45 @@
+import { ANTAGONIST } from "antagonist";
+import { CONNECT } from "connect";
+import { createTimestamp } from "create-timestamp";
+import { createUuid } from "uuid";
 import { createConnectOrder } from "./create-connect-order";
+
+const mockUuid = "9e45ac88-4a1e-4c88-9f73-7a2966029182";
+
+jest.mock("uuid");
+
+const mockCreateUuid = jest.mocked(createUuid);
+
+mockCreateUuid.mockReturnValue(mockUuid);
+
+const mockTimestamp = Date.now();
+
+jest.mock("create-timestamp");
+
+const mockCreateTimestamp = jest.mocked(createTimestamp);
+
+mockCreateTimestamp.mockReturnValue(mockTimestamp);
 
 describe("The createConnectOrder function", () => {
   test.each([
-    [{}, {}],
-    [{}, {}],
+    [
+      {
+        entityId: "13d20d0e-f9fa-4e82-a65b-794a31382795",
+        role: ANTAGONIST,
+      },
+      {
+        id: mockUuid,
+        entityId: "13d20d0e-f9fa-4e82-a65b-794a31382795",
+        name: CONNECT,
+        role: ANTAGONIST,
+        timestamp: mockTimestamp,
+      },
+    ],
   ] as [Parameters<typeof createConnectOrder>[0], ReturnType<typeof createConnectOrder>][])(
     "should return the expected result.",
     (validExample, expectedResult) => {
       const result = createConnectOrder(validExample);
+
       expect(result).toStrictEqual(expectedResult);
     }
   );
--- src/create-disconnect-order/create-disconnect-order.test.ts
+++ src/create-disconnect-order/create-disconnect-order.test.ts
@@ -1,13 +1,42 @@
+import { createTimestamp } from "create-timestamp";
+import { DISCONNECT } from "disconnect";
+import { createUuid } from "uuid";
 import { createDisconnectOrder } from "./create-disconnect-order";
+
+const mockUuid = "9e45ac88-4a1e-4c88-9f73-7a2966029182";
+
+jest.mock("uuid");
+
+const mockCreateUuid = jest.mocked(createUuid);
+
+mockCreateUuid.mockReturnValue(mockUuid);
+
+const mockTimestamp = Date.now();
+
+jest.mock("create-timestamp");
+
+const mockCreateTimestamp = jest.mocked(createTimestamp);
+
+mockCreateTimestamp.mockReturnValue(mockTimestamp);
 
 describe("The createDisconnectOrder function", () => {
   test.each([
-    [{}, {}],
-    [{}, {}],
+    [
+      {
+        entityId: "13d20d0e-f9fa-4e82-a65b-794a31382795",
+      },
+      {
+        id: mockUuid,
+        entityId: "13d20d0e-f9fa-4e82-a65b-794a31382795",
+        name: DISCONNECT,
+        timestamp: mockTimestamp,
+      },
+    ],
   ] as [Parameters<typeof createDisconnectOrder>[0], ReturnType<typeof createDisconnectOrder>][])(
     "should return the expected result.",
     (validExample, expectedResult) => {
       const result = createDisconnectOrder(validExample);
+
       expect(result).toStrictEqual(expectedResult);
     }
   );
