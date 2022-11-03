ac6479584d9c8eced0f855c4528f3d83d06ae00f
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-03 18:49:17 -0400

    explicitly specify return type

--- src/handle-orders/handle-orders.ts
+++ src/handle-orders/handle-orders.ts
@@ -10,7 +10,7 @@ export const handleOrders = ({
   componentIndex,
 }: {
   componentIndex: ComponentIndex;
-}) => {
+}): ComponentIndex => {
   const orderHandlers = getOrderHandlers();
 
   return orderHandlers.reduce<ComponentIndex>(
