d42eade01ff5a65ad9554fb938e7d44d73d9b662
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-06 12:05:48 -0500

    remove expired components before handling orders

--- src/get-new-components/get-new-components.ts
+++ src/get-new-components/get-new-components.ts
@@ -1,6 +1,7 @@
 import {Component} from "component";
 import {ComponentIndex} from "component-index";
 import {handleOrders} from "handle-orders";
+import {removeExpiredComponents} from "remove-expired-components";
 
 /**
  * Derive a {@link Component} array representing next state of the game from a {@link ComponentIndex} representing the current state of the game.
@@ -11,7 +12,13 @@ export const getNewComponents = ({
 }: {
   componentIndex: ComponentIndex;
 }): Component[] => {
-  componentIndex = handleOrders({ componentIndex });
+  const componentIndexWithoutExpiredComponents = removeExpiredComponents({
+    componentIndex,
+  });
 
-  return Object.values(componentIndex.byComponentId);
+  const componentIndexAfterHandlingOrders = handleOrders({
+    componentIndex: componentIndexWithoutExpiredComponents,
+  });
+
+  return Object.values(componentIndexAfterHandlingOrders.byComponentId);
 };
