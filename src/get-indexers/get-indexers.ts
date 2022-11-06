9545e0efc7a2fccf427a22f3ffe28e84a09b093d
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-06 16:10:01 -0500

    add isPingOrder to indexers

--- src/get-indexers/get-indexers.ts
+++ src/get-indexers/get-indexers.ts
@@ -2,10 +2,11 @@ import {ComponentIndexer} from "component-indexer";
 import {isConnectOrder} from "connect-order";
 import {isDisconnectOrder} from "disconnect-order";
 import {isExpired} from "expired";
+import {isPingOrder} from "ping-order";
 
 /**
  * Return an array of {@link ComponentIndexer} functions.
  */
 export const getIndexers = (): ComponentIndexer[] => {
-  return [isConnectOrder, isDisconnectOrder, isExpired];
+  return [isConnectOrder, isDisconnectOrder, isExpired, isPingOrder];
 };
